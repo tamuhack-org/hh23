7f2fc4c615d4e6b612a59be878678c2085ccc3aa
Adam Teo <adam.teo321@gmail.com>
2023-06-24 11:59:13 -0400

    navbar, mountain + stars background

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dark-purple': "#2D284C",
+        'pale-yellow': "#FFCC67",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'mountain-stars': "url('/assets/mountain-stars.svg')" 
       },
     },
   },
