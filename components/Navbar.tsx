9a9d5274f2f32a665349b8c03b9f3e3e34fbcfd5
Adam Teo <adam.teo321@gmail.com>
2023-07-15 21:39:38 -0400

    smooth scroll to sections

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,32 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Navbar = () => {
   return (
     <div className="hidden lg:flex items-center justify-between px-8 xl:px-24 py-4 w-full bg-dark-purple z-50 text-pale-yellow border-b-3 border-pale-yellow">
       <div className="w-1/3">
-        <Link href="/"
+        <AnchorLink href="/"
           className="text-3xl rounded-full font-black font-heading navy">
           <Image src="/assets/hh-logo.png" width={50} height={50} alt="hh-logo" />
-        </Link>
+        </AnchorLink>
       </div>
       <div className="flex justify-center items-center gap-8 w-1/3 font-bold text-lg">
-        <Link href="#info">
+        <AnchorLink href="#info">
           Info
-        </Link>
-        <Link href="#faq">
+        </AnchorLink>
+        <AnchorLink href="#faq">
           FAQ
-        </Link>
-        <Link href="#schedule">
+        </AnchorLink>
+        <AnchorLink href="#schedule">
           Schedule
-        </Link>
-        <Link href="#prizes">
+        </AnchorLink>
+        <AnchorLink href="#prizes">
           Prizes
-        </Link>
-        <Link href="#resources">
+        </AnchorLink>
+        <AnchorLink href="#resources">
           Resources
-        </Link>
+        </AnchorLink>
       </div>
       <div className="w-1/3 flex justify-end items-center">
         <Link href="https://tamuhack.org" target="_blank" rel="noopener" className="xl:flex text-4xl font-bold mr-8 ">
